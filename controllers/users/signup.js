5711d650a690c41418b86f4f11bf803805b37037
JackGre <gl.meh.bmes.od@gmail.com>
2021-12-21 16:36:35 +0200

    good

---
+++ controllers/users/signup.js
@@ -0,0 +1,31 @@
+const { Conflict } = require('http-errors')
+const gravatar = require('gravatar')
+const bcrypt = require('bcryptjs')
+
+const { User } = require('../../models')
+
+const signup = async (req, res) => {
+  const { password, email, subscription, token } = req.body
+  const user = await User.findOne({ email })
+  if (user) {
+    throw new Conflict('Email in use')
+  }
+  const avatarURL = gravatar.url(email)
+
+  const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10))
+
+  const result = await User.create({ password: hashPassword, email, subscription, token, avatarURL })
+  res.status(201).json({
+    status: 'success',
+    code: 201,
+    data: {
+      user: {
+        email,
+        subscription,
+        avatarURL
+      }
+    }
+  })
+}
+
+module.exports = signup
